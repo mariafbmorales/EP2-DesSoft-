340ea8b241cb6c97a24bb97abaa9ab2f530b9858
Giovana <giovananzg@al.edu.insper.br>
2025-04-27 20:16:03 -0300

    questao 2

--- funcoes.py
+++ funcoes.py
@@ -5,3 +5,15 @@ def rolar_dados(qnt_dados):
         sorteado = random.randint(1, 6)
         dados_rolados.append(sorteado)
     return dados_rolados
+
+def guardar_dado(lista_rolados, lista_guardados, indice):
+    lista_final = []
+    nova_lista_rolados = []
+    for i in range(len(lista_rolados)):
+        if lista_rolados[i] != lista_rolados[indice]:
+            nova_lista_rolados.append(lista_rolados[i])
+        lista_final.append(nova_lista_rolados)
+    lista_guardados.append(lista_rolados[indice])
+    lista_final.append(lista_guardados)
+    return lista_final
+
