eae1ae3f182290c7f20c67adba7a7f54c07ca631
Giovana <giovananzg@al.edu.insper.br>
2025-04-27 20:21:56 -0300

    questao 2.2

--- funcoes.py
+++ funcoes.py
@@ -7,13 +7,10 @@ def rolar_dados(qnt_dados):
     return dados_rolados
 
 def guardar_dado(lista_rolados, lista_guardados, indice):
-    lista_final = []
+    lista_guardados.append(lista_rolados[indice])
     nova_lista_rolados = []
     for i in range(len(lista_rolados)):
-        if lista_rolados[i] != lista_rolados[indice]:
+        if i != indice:
             nova_lista_rolados.append(lista_rolados[i])
-        lista_final.append(nova_lista_rolados)
-    lista_guardados.append(lista_rolados[indice])
-    lista_final.append(lista_guardados)
-    return lista_final
+    return [nova_lista_rolados, lista_guardados]
 
