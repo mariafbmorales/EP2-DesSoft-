857c5210d3161ac0a9af84f2d4760356a965deec
Giovana <giovananzg@al.edu.insper.br>
2025-05-06 09:48:42 -0300

    funcoes importadas

---
+++ programa.py
@@ -0,0 +1,14 @@
+from funcoes import rolar_dados
+from funcoes import guardar_dado
+from funcoes import remover_dado
+from funcoes import calcula_pontos_regra_simples
+from funcoes import calcula_pontos_soma 
+from funcoes import calcula_pontos_sequencia_baixa
+from funcoes import calcula_pontos_sequencia_alta 
+from funcoes import calcula_pontos_full_house
+from funcoes import calcula_pontos_quadra 
+from funcoes import calcula_pontos_quina
+from funcoes import calcula_pontos_regra_avancada 
+from funcoes import faz_jogada
+from funcoes import imprime_cartela
+
