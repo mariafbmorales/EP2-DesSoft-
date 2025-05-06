af0cb783561f3ae2dd4e8dda3efbdf02609b9ecb
Maria <nanda031006@gmail.com>
2025-05-06 17:47:12 -0300

    questao 13

--- programa.py
+++ programa.py
@@ -12,3 +12,10 @@ from funcoes import calcula_pontos_regra_avancada
 from funcoes import faz_jogada
 from funcoes import imprime_cartela
 
+cartela_de_pontuação = {'regra_simples': {1: -1, 2: -1, 3: -1, 4: -1, 5: -1, 6: -1},'regra_avancada': {'sem_combinacao': -1,'quadra': -1,'full_house': -1,'sequencia_baixa': -1,'sequencia_alta': -1,'cinco_iguais':-1}}
+imprime_cartela(cartela_de_pontuação)
+reg_simp = ["1", "2", "3", "4", "5", "6"]
+reg_av = ["sem_combinacao","quadra","full_house","sequencia_baixa","sequencia_alta","cinco_iguais"]
+
+for i in range (12):
+    
